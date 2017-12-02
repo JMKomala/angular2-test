03a9eb2c261856b5980b6cdd7e1a2e0297928894
JM_Komala <jmkomala@gmail.com>
2017-12-02 15:11:28 -0800

    data binding

--- confusion/src/app/app.module.ts
+++ confusion/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ confusion/src/app/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+// import { DishDetailComponent } from '../dish-detail/dish-detail.component';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  dishes: Dish[] = DISHES;
+  selectedDish = Dish;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect = (dish) => {
+  this.selectedDish= dish;
+  }
+
+}
